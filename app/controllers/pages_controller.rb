1e31a708d875dc43e36d2cd6ac01f4c867829169
davidrzuluaga <git@davidrzuluaga.com>
2018-08-08 20:54:46 -0500

    parte de los views terminado

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,18 @@
 class PagesController < ApplicationController
+
   def home
+    
+  end
+
+  def about_us
+    @about = "Lorem Ipsum"
+  end
+
+  def greet
+    @greeting = "David R Zuluaga"
+  end
+
+  def contact
+    @contacts = [David, 3019002134]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   get 'pages/home'
-  root 'pages#freaking'
+  root 'pages#home'
+
+  get 'pages/about_us'
+  get 'pages/greet'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
